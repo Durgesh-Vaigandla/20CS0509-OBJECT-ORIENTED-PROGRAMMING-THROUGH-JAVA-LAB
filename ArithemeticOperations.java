ccdc31a6a76e175b5bdcd42d4a572deaaa6d6adc
Durgesh <vvip202014@gmail.com>
2024-02-07 20:44:42 +0530

    Add ArithemeticOperations, AssignmentOperators, and Helloworld classes

---
+++ ArithemeticOperations.java
@@ -0,0 +1,10 @@
+public class ArithemeticOperations {
+    public static void main(String[] args) {
+        System.out.println("Arithemetic Operations:-");
+        System.out.println("5 + 2 = "+(5+2));
+        System.out.println("5 - 2 = "+(5-2));
+        System.out.println("5 * 2 = "+(5*2));
+        System.out.println("5 / 2 = "+(5/2));
+        System.out.println("5 % 2 = "+(5%2));
+    }
+}
