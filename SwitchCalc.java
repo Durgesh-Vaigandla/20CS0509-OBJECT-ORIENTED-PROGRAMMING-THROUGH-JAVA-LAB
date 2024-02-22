f99d2592c3ea33ad427fc4ad854e4aefbefad08c
Durgesh <vvip202014@gmail.com>
2024-02-22 12:11:17 +0530

    Add SwitchCalc.java - A calculator program that performs basic arithmetic operations using a switch statement.

---
+++ SwitchCalc.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class SwitchCalc {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Enter second number: ");
+        int num2 = scanner.nextInt();
+        System.out.print("Enter operator (+, -, *, /, %): ");
+        char operator = scanner.next().charAt(0);
+        switch (operator) {
+            case '+':
+                System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
+                break;
+            case '-':
+                System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));
+                break;
+            case '*':
+                System.out.println(num1 + " * " + num2 + " = " + (num1 * num2));
+                break;
+            case '/':
+                if (num2 == 0) {
+                    System.out.println("Division by zero is not allowed.");
+                } else {
+                    System.out.println(num1 + " / " + num2 + " = " + ((double) num1 / num2));
+                }
+                break;
+            case '%':
+                System.out.println(num1 + " % " + num2 + " = " + (num1 % num2));
+                break;
+            default:
+                System.out.println("Invalid operator entered.");
+        }
+        scanner.close();
+    }
+}
+
