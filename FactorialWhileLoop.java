a5c8f65c42d2addc42061dc05f40b3cc9b05c454
DURGESH VAIGANDLA <150946641+Durgesh-Vaigandla@users.noreply.github.com>
2024-02-23 10:06:25 +0530

    Update FactorialWhileLoop.java

--- FactorialWhileLoop.java
+++ FactorialWhileLoop.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class FactorialWhileLoop {
     public static void main(String[] args) {
       Scanner input = new Scanner(System.in);
-        int number = input.nextInt;
+        int number = input.nextInt();
         int factorial = 1;
         int i = 1;
         while (i <= number) {
